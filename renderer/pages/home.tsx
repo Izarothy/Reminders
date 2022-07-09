9d2d533e8980b9e22fbee16e4cb83d991bcb53e2
Izarothy <izarothmail@gmail.com>
2022-07-09 17:14:53 +0200

    Change the window's title

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 import ReminderForm from '../components/ReminderForm';
-
 function Home() {
   return (
     <>
       <Head>
-        <title>Home - Nextron (with-typescript-tailwindcss)</title>
+        <title>Reminders</title>
       </Head>
       <div className="grid place-items-center h-screen">
         <ReminderForm />
