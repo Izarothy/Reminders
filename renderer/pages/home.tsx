6bf14eed5506d6e55218ea624ec9ba1132ff5b04
Izarothy <izarothmail@gmail.com>
2022-07-09 13:29:22 +0200

    Initial setup with create nextron app

---
+++ renderer/pages/home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home - Nextron (with-typescript-tailwindcss)</title>
+      </Head>
+      <body className="grid place-items-center h-screen">
+        <h1>Test</h1>
+      </body>
+    </>
+  );
+}
+
+export default Home;
