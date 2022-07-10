e7404ec578b23b3ae3dfaada3ebb407872fb08aa
Izarothy <izarothmail@gmail.com>
2022-07-10 19:15:06 +0200

    Move app title to all pages

--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <title>Reminders</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -4,9 +4,6 @@ import ReminderForm from '../components/ReminderForm';
 function Home() {
   return (
     <>
-      <Head>
-        <title>Reminders</title>
-      </Head>
       <div className="grid place-items-center h-screen">
         <ReminderForm />
       </div>
