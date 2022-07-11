26fd2b448faf3e97da8acd19aa0018b15a1a0944
Izarothy <izarothmail@gmail.com>
2022-07-11 15:40:44 +0200

    Move the page's title from document to app

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from 'next/app';
 import { store } from '../redux/store';
 import '../styles/globals.css';
 import { Provider } from 'react-redux';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Reminders</title>
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title>Reminders</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
