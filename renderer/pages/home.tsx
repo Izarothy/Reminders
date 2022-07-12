4024e5d982164266ac9fef8300527c56145b4c43
Izarothy <izarothmail@gmail.com>
2022-07-12 22:42:54 +0200

    Remove unused next/head import, formatting

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import Head from 'next/head';
 import ReminderForm from '../components/ReminderForm';
+
 function Home() {
   return (
-    <>
-      <div className="grid place-items-center h-screen">
-        <ReminderForm />
-      </div>
-    </>
+    <div className="grid place-items-center h-screen">
+      <ReminderForm />
+    </div>
   );
 }
 
